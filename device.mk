c71c827494e5c561aa8ae7c97e3ce5a1d9c82558
HackerOO7 <x2280854@gmail.com>
2015-06-14 19:41:30 +0800

    xt1085: add wlan firmware from victara china
    Change-Id: I9144bbc316f5c2f026882a336f9a666563581972

--- device.mk
+++ device.mk
@@ -253,7 +253,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_tqnt_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_tqnt_nv.bin
 
 # CMActions
 PRODUCT_PACKAGES += \
