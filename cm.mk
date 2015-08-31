1ab9dd978fd7ca78dc1c818ff019432dd44743dd
HackerOO7 <x2280854@gmail.com>
2015-08-31 10:24:54 +0800

    xt1085: update for 5.1 and rm duplicate proprietary
    Change-Id: Iea5fc4dd5a9d978adf77370ea08d4c505c1af964

--- cm.mk
+++ cm.mk
@@ -11,10 +11,11 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
 PRODUCT_NAME := cm_victara
+PRODUCT_MODEL := XT1085
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=victara_retcn \
-    BUILD_FINGERPRINT=motorola/victara_retcn/victara:5.0.2/LXE22.92-30/30:user/release-keys \
-    PRIVATE_BUILD_DESC="victara_retcn-user 5.0.2 LXE22.92-30 30 release-keys"
+    BUILD_FINGERPRINT=motorola/victara_retcn/victara:5.1/LPE23.32-53/41:user/release-keys \
+    PRIVATE_BUILD_DESC="victara_retcn-user 5.1 LPE23.32-53 41 release-keys"
